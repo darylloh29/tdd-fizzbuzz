1606e3728bb482d3327c7af579729f2114a0c9cb
Daryl <lohxianjun@gmail.com>
2024-05-06 18:11:02 +0800

    Add test case for multiples of 3 to return Fizz

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -4,4 +4,7 @@ describe("fizzbuzz", () => {
     test("input of 1 returns 1", () => {
         expect(fizzbuzz(1)).toBe("1");
     });
+    test("input of 3 returns Fizz", () => {
+        expect(fizzbuzz(3)).toBe("Fizz");
+    });
 });
