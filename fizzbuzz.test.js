408fae5ef712bfafcea7045e784b49d793cc27f7
Daryl <lohxianjun@gmail.com>
2024-05-06 18:23:05 +0800

    Add test case for negative values

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,16 +1,23 @@
 const fizzbuzz = require("./fizzbuzz");
 
 describe("fizzbuzz", () => {
-    test("input of 1 returns 1", () => {
-        expect(fizzbuzz(1)).toBe("1");
+    describe("valid input", () => {
+        test("input of 1 returns 1", () => {
+            expect(fizzbuzz(1)).toBe("1");
+        });
+        test("input of 3 returns Fizz", () => {
+            expect(fizzbuzz(3)).toBe("Fizz");
+        });
+        test("input of 5 returns Buzz", () => {
+            expect(fizzbuzz(5)).toBe("Buzz");
+        });
+        test("input of 15 returns FizzBuzz", () => {
+            expect(fizzbuzz(15)).toBe("FizzBuzz");
+        });
     });
-    test("input of 3 returns Fizz", () => {
-        expect(fizzbuzz(3)).toBe("Fizz");
-    });
-    test("input of 5 returns Buzz", () => {
-        expect(fizzbuzz(5)).toBe("Buzz");
-    });
-    test("input of 15 returns FizzBuzz", () => {
-        expect(fizzbuzz(15)).toBe("FizzBuzz");
+    describe("invalid input", () => {
+        test("input of -1 returns Invalid input", () => {
+            expect(fizzbuzz(-1)).toBe("Invalid input");
+        });
     });
 });
